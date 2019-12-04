0d4ffc7e10384530d964df7e21e582ed0e7449a1
Farhad009 <58331948+Farhad009@users.noreply.github.com>
2019-12-04 01:36:09 -0800

    Add files via upload

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right">
+            <p>
+                        &copy; Copyright, {{ date('Y') }} <a href="#">Food Cornner</a>
+                        
+                    </p>
+        </p>
+    </div>
+</footer>
